d3d1c8db64d66146424546883852b74eff3d3d36
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-08 16:11:59 -0300

    chore: upload Spring Boot starter template application to init migration

---
+++ src/test/java/com/laporeon/user_registration_system/UserRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laporeon.user_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
