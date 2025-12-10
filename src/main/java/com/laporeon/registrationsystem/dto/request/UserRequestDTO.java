248838bc10a53be4ccc391cea828712bd7e335ce
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-10 15:25:28 -0300

    feat(dto): add example values for email and username at UserRequestDTO

--- src/main/java/com/laporeon/registrationsystem/dto/request/UserRequestDTO.java
+++ src/main/java/com/laporeon/registrationsystem/dto/request/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package com.laporeon.registrationsystem.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -7,9 +8,11 @@ import jakarta.validation.constraints.Size;
 public record UserRequestDTO(
         @NotBlank(message = "Username is required")
         @Size(min = 6, max = 25, message = "Username must be between {min} and {max} characters")
+        @Schema(example = "username")
         String username,
         @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Invalid email")
         @NotBlank(message = "Email is required")
+        @Schema(example = "user@gmail.com")
         String email
 ) {
 }
