a111765500b8999c9f1326fe6c8559021f7f38d8
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-10 13:54:49 -0300

    refactor(dto): replace @Email with @Pattern to improve email validation and add @NotBlank constraint

--- src/main/java/com/laporeon/registrationsystem/dto/request/UserRequestDTO.java
+++ src/main/java/com/laporeon/registrationsystem/dto/request/UserRequestDTO.java
@@ -1,14 +1,15 @@
 package com.laporeon.registrationsystem.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserRequestDTO(
         @NotBlank(message = "Username is required")
-        @Size(min = 6, max = 25, message = "Username must be {min} and {max} characters long. Please try again.")
+        @Size(min = 6, max = 25, message = "Username must be between {min} and {max} characters")
         String username,
-        @Email(message = "Invalid email")
+        @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Invalid email")
+        @NotBlank(message = "Email is required")
         String email
 ) {
 }
