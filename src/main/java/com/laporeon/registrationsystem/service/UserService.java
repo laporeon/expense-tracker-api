ccaccb62c022b5ddf2648d01d24d56846587d397
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-10 14:52:06 -0300

    feat(service): implement method to ensure unique fields before saving User entity

--- src/main/java/com/laporeon/registrationsystem/service/UserService.java
+++ src/main/java/com/laporeon/registrationsystem/service/UserService.java
@@ -3,6 +3,7 @@ package com.laporeon.registrationsystem.service;
 import com.laporeon.registrationsystem.dto.request.UserRequestDTO;
 import com.laporeon.registrationsystem.dto.response.UserResponseDTO;
 import com.laporeon.registrationsystem.entity.User;
+import com.laporeon.registrationsystem.exception.AlreadyRegisteredException;
 import com.laporeon.registrationsystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserResponseDTO createUser(UserRequestDTO dto) {
+        ensureUniqueFields(dto);
+
         User user = User.builder()
                         .username(dto.username())
                         .email(dto.email())
@@ -28,5 +31,19 @@ public class UserService {
                 user.getCreatedAt(),
                 user.getUpdatedAt()
         );
+    }
+
+    private void ensureUniqueFields(UserRequestDTO dto) {
+        if (userRepository.existsByUsernameAndEmail(dto.username(), dto.email())) {
+            throw new AlreadyRegisteredException("Email and username already in use");
+        }
+
+        if (userRepository.existsByEmail(dto.email())) {
+            throw new AlreadyRegisteredException("Email already registered");
+        }
+
+        if (userRepository.existsByUsername(dto.username())) {
+            throw new AlreadyRegisteredException("Username already taken");
+        }
     }
 }
