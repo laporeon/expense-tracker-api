4248f434dc05c645787c77794aa05a1212d8b8ff
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-10 13:56:41 -0300

    feat(exception): start implementation of GlobalExceptionHandler class

---
+++ src/main/java/com/laporeon/registrationsystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.laporeon.registrationsystem.exception;
+
+import com.laporeon.registrationsystem.dto.response.ErrorResponseDTO;
+import com.laporeon.registrationsystem.dto.response.ValidationErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> details = ex.getBindingResult()
+                                        .getFieldErrors()
+                                        .stream()
+                                        .collect(Collectors.toMap(
+                                                FieldError::getField,
+                                                FieldError::getDefaultMessage));
+
+        ValidationErrorResponseDTO error = new ValidationErrorResponseDTO(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation failed",
+                details,
+                Instant.now());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception ex) {
+        log.error("An unexpected error occurred {}", ex.getMessage());
+
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred",
+                Instant.now());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
