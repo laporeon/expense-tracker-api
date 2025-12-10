2e9158ccd6836b7bd7aa6afea00a6b1c69f968f1
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-10 14:51:12 -0300

    feat(repository): add methods to ensure unique email and username fields

--- src/main/java/com/laporeon/registrationsystem/repository/UserRepository.java
+++ src/main/java/com/laporeon/registrationsystem/repository/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+   boolean existsByUsernameAndEmail(String username, String email);
+   boolean existsByEmail(String email);
+   boolean existsByUsername(String username);
 }
