2e5102cefb59f00734d870b360651a45533807b6
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-15 15:16:55 -0300

    feat(dto): add password as optional field for update user request body

--- src/main/java/com/laporeon/expensetracker/dto/request/UpdateUserRequestDTO.java
+++ src/main/java/com/laporeon/expensetracker/dto/request/UpdateUserRequestDTO.java
@@ -1,13 +1,23 @@
 package com.laporeon.expensetracker.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record UpdateUserRequestDTO(
         @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "Invalid email")
-        @NotBlank(message = "Email is required")
-        @Schema(example = "user@gmail.com")
-        String email
+        @Schema(
+                description = "Optional email. Must be a valid email address.",
+                example = "user@gmail.com"
+        )
+        String email,
+        @Pattern(
+                regexp = "^(?=\\S{8,25}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).*$",
+                message = "Password must be 8-25 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character."
+        )
+        @Schema(
+                description = "Optional password. Must follow the defined complexity rules.",
+                example = "#P4ssword_"
+        )
+        String password
 ) {
 }
