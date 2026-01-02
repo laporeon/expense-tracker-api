54ceaf4dcf7e8de994800b6b17906b8fef3483d3
laporeon <175569928+laporeon@users.noreply.github.com>
2026-01-02 16:04:53 -0300

    refactor(auth): change login parameter to email since username is no longer entity attribute

--- src/main/java/com/laporeon/expensetracker/services/AuthService.java
+++ src/main/java/com/laporeon/expensetracker/services/AuthService.java
@@ -5,7 +5,8 @@ import com.laporeon.expensetracker.dtos.request.RegisterRequestDTO;
 import com.laporeon.expensetracker.dtos.response.LoginResponseDTO;
 import com.laporeon.expensetracker.dtos.response.RegisterResponseDTO;
 import com.laporeon.expensetracker.entities.User;
-import com.laporeon.expensetracker.helpers.CustomValidator;
+import com.laporeon.expensetracker.exceptions.AlreadyRegisteredException;
+import com.laporeon.expensetracker.helpers.JwtTokenProvider;
 import com.laporeon.expensetracker.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,39 +21,38 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-    private final CustomValidator customValidator;
     private final AuthenticationManager authenticationManager;
-    private final TokenService tokenService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
     public RegisterResponseDTO register(RegisterRequestDTO dto) {
-        customValidator.ensureUniqueFields(dto.username(), dto.email());
+        if (userRepository.existsByEmail(dto.email())) {
+            throw new AlreadyRegisteredException("Email already registered");
+        }
 
         User user = User.builder()
                         .name(dto.name())
-                        .username(dto.username())
                         .email(dto.email())
                         .password(passwordEncoder.encode(dto.password()))
                         .build();
 
         userRepository.save(user);
 
-        String token = tokenService.generateToken(user);
+        String token = jwtTokenProvider.generateToken(user);
 
         return new RegisterResponseDTO(
                 user.getId(),
                 user.getName(),
-                user.getUsername(),
                 user.getEmail(),
                 token
         );
     }
 
     public LoginResponseDTO login(LoginRequestDTO dto) {
-        var loginPassword = new UsernamePasswordAuthenticationToken(dto.login(), dto.password());
+        var loginPassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
         User user = (User) authenticationManager.authenticate(loginPassword).getPrincipal();
 
-        String token = tokenService.generateToken(user);
+        String token = jwtTokenProvider.generateToken(user);
 
         return new LoginResponseDTO(token);
     }
--- src/main/java/com/laporeon/expensetracker/services/CustomUserDetailsService.java
+++ src/main/java/com/laporeon/expensetracker/services/CustomUserDetailsService.java
@@ -14,8 +14,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        return userRepository.findByLogin(login)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
                              .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
